c2c524c40d4869419d9556d8af1aae3e0897a165
Rahman Hakim <rahmanhakim2435@gmail.com>
2025-05-05 19:35:25 +0700

    Update cors

--- digivise/src/main/java/org/nevmock/digivise/DigiviseApplication.java
+++ digivise/src/main/java/org/nevmock/digivise/DigiviseApplication.java
@@ -2,6 +2,9 @@ package org.nevmock.digivise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DigiviseApplication {
@@ -9,6 +12,16 @@ public class DigiviseApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(DigiviseApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api").allowedOrigins("http://localhost:5173");
+            }
+        };
     }
 
 }
